f0798709505ded91ea80fb8301211212688c0234
swetharamayanam <swetharamayanam@gmail.com>
2021-12-30 07:03:25 +0100

    Python_intro and string files are uploading

---
+++ HelloWorld/python_topics/python_intro.py
@@ -0,0 +1,15 @@
+# intro :
+# used for web development, software development, mathematics, system scripting.
+# used on a server to create web applications, used alongside to create workflows.
+# can connect to dbs and can read and modify files.
+# used to handle big data and perform complex mathematics.
+# used for rapid prototyping, for production ready software development.
+# possible to write python in IDE( Integrated Development Environment).
+
+print("Hello World!")
+exit()
+
+# output : Hello World!
+# need to revise python get started, did not understand how to give command and check.
+
+
