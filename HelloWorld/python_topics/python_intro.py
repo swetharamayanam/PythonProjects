2e40501da0b7a9f83f3ab054e5a40d94e3d000f3
swetharamayanam <swetharamayanam@gmail.com>
2021-12-31 06:31:48 +0100

    python introduction notes

--- HelloWorld/python_topics/python_intro.py
+++ HelloWorld/python_topics/python_intro.py
@@ -10,6 +10,31 @@ print("Hello World!")
 exit()
 
 # output : Hello World!
-# need to revise python get started, did not understand how to give command and check.
+
+
+# syntax : syntax will start with print and and ends with brackets.
+# mentioning various types of variables and to run the program syntax is required.
+# can store integer, float, string or complex numbers in variables.
+
+# comments : will start with #. this can be useful to understand the code clearly.
+# comments are not come under code.
+# if the comment is more than one line, we can write the entire comment in three quotes.
+
+
+# variable : variable is to store some data values.
+# the data might be in the form of int, float, string.
+# to know the data type we can write a syntax.
+# we can mention string data type in single or double quotes.
+# if it is more than one more line we can write in three quotes.
+# variable names can be case sensitive, alpha-numeric characters and underscores.
+# variable names cannot starts with any symbols like @ , $ ,etc and cannot starts with numeric.
+# variable can be camel case (myVariableName) or pascal case(MyVariableName) or snake case(my_variable_case).
+
+# Assign multiple variables :
+# we can assign multiple variables in one line.
+# and we can assign the same value to multiple variables in one line.
+
+
+
 
 
